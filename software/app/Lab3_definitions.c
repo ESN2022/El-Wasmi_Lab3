3f89dcfaff6d7dce870b75914b215bdf2f8e866b
AhmedWasmi <75042681+AhmedWasmi@users.noreply.github.com>
2023-01-18 11:12:49 +0100

    3 comit: amelioration du code c: - lecture et affichage de toute les axes dans le terminal et les afficheur 7 segment - ajout des switch pour selectionner quelle axe est afficher dans l'afficheur - integration de timer en interuption qui demare chaque seconde pour lire le capteur
    - Ajout de la calibration des valeurs de l'accéléromètre en utilisant les registres d'offset du capteur, en lisant la datasheet du capteur pour comprendre comment les utiliser correctement.
    - Intégration de la lecture de tous les axes de l'accéléromètre (X, Y, Z) et affichage des valeurs dans le terminal et sur les afficheurs à 7 segments.
    - Ajout de Switchs pour sélectionner quel axe afficher sur les afficheurs à 7 segments.
    - Intégration d'un timer en interruption qui démarre chaque seconde pour lire les valeurs de l'accéléromètre.

--- software/app/Lab3_definitions.c
+++ software/app/Lab3_definitions.c
@@ -5,20 +5,55 @@
  *      Author: Ahmed
  */
 
-#include <stdio.h>
 #include "Lab3_definitions.h"
 
 void write_register(alt_16 x,int value) {
 	   I2C_start(I2C_BASE, 0x1D, 0);
-	   I2C_write(I2C_BASE, x, 0);
+	    I2C_write(I2C_BASE, x, 0);
 	    I2C_write(I2C_BASE, value, 1);
+
 }
 
 
-alt_16 read_register(alt_16 x) {
-	 I2C_start(I2C_BASE, 0x1D, 0);
-		    I2C_write(I2C_BASE, x, 0);
-	   I2C_start(I2C_BASE, 0x1D, 1);
-	   return I2C_read(I2C_BASE, 1);
+int read_register(alt_16 x) {
+	I2C_start(I2C_BASE, 0x1D, 0);
+	I2C_write(I2C_BASE, x, 0);
+	I2C_start(I2C_BASE, 0x1D, 1);
+	return I2C_read(I2C_BASE, 1);
 
+
+}
+
+
+
+alt_32 int_to_bcd(alt_32 int_value)
+{
+	alt_32 result=0;
+    if (int_value < 0) {
+        int_value = -int_value;
+        result=15<<16;
+    }
+    else{ result=14<<16;}
+    char bcd_value[4];
+    bcd_value[0] = int_value % 10;
+    bcd_value[1] = int_value / 10 % 10;
+    bcd_value[2] = int_value / 100 % 10;
+    bcd_value[3] = int_value / 1000 % 10;
+    return result=result+(bcd_value[3] << 12)+(bcd_value[2] << 8) + (bcd_value[1] << 4) + bcd_value[0];
+}
+
+void axes() {
+    switch(switch_value) {
+        case 0b00:
+        	  IOWR_ALTERA_AVALON_PIO_DATA(SEG_BASE, (10 << 20)+int_to_bcd(X * 4));
+            printf("Valeur pour switch 00: x\n");
+            break;
+        case 0b01:
+        	  IOWR_ALTERA_AVALON_PIO_DATA(SEG_BASE, (11 << 20)+int_to_bcd(Y * 4));
+            printf("Valeur pour switch 01: y\n");
+            break;
+        case 0b10:
+        	  IOWR_ALTERA_AVALON_PIO_DATA(SEG_BASE, (12 << 20)+int_to_bcd(Z * 4));
+            printf("Valeur pour switch 10: z\n");
+            break;}
 }
--- software/app/main.c
+++ software/app/main.c
@@ -1,39 +1,68 @@
-#include <stdio.h>
-#include "system.h"
 #include "Lab3_definitions.h"
-#include "opencores_i2c.h"
 
 
+static void timer_interrupt(void * Context, alt_u32 id);
+static void irqhandler_switch(void * context);
 
 int main() {
-  int data[6];
-  int i;
 
   I2C_init(I2C_BASE, ALT_CPU_FREQ, 100000);
 
   if (I2C_start(I2C_BASE, 0x1D, 0) == 0) {
-    printf("connection etablie");
+    printf("connection établie\n");
   } else {
-    printf("error de connexion");
+    printf("error de connexion\n");
   }
+
+  IOWR_ALTERA_AVALON_TIMER_STATUS(TIMER_0_BASE, 0);
+  alt_ic_isr_register(TIMER_0_IRQ_INTERRUPT_CONTROLLER_ID, TIMER_0_IRQ, (void * ) timer_interrupt, NULL, 0);
+  IOWR_ALTERA_AVALON_TIMER_CONTROL(TIMER_0_BASE, 0x07);
+
+  IOWR_ALTERA_AVALON_PIO_IRQ_MASK(SWITCHS_BASE, 0b1111);
+  IOWR_ALTERA_AVALON_PIO_EDGE_CAP(SWITCHS_BASE, 0x0);
+  alt_ic_isr_register(SWITCHS_IRQ_INTERRUPT_CONTROLLER_ID, SWITCHS_IRQ, (void * ) irqhandler_switch, NULL, NULL);
 
   write_register(ACT_INACT_CTL, 240);
   write_register(POWER_CT, 8);
   write_register(DATA_FORMAT, 8);
+  write_register(ADXL345_OFSX, 0);
+  write_register(ADXL345_OFSY, -1);
+  write_register(ADXL345_OFSZ, 0);
 
-  while (1) {
-    // lecture de l'axe x
+  switch_value = IORD_ALTERA_AVALON_PIO_DATA(SWITCHS_BASE);
 
-    data[0] = read_register(ADXL345_DATAX0);
-
-    data[1] = read_register(ADXL345_DATAX1);
-
-    alt_16 x = (data[1] << 8) | data[0];
-
-    printf("X: %d \n", x * 4);
-
-    // Delay for a bit
-    usleep(1000000);
-  }
+  while (1);
   return 0;
+}
+
+static void timer_interrupt(void * Context, alt_u32 id) {
+
+  data[0] = read_register(ADXL345_DATAX0);
+
+  data[1] = read_register(ADXL345_DATAX1);
+
+  X = (data[1] << 8) | data[0];
+
+  data[2] = read_register(ADXL345_DATAY0);
+
+  data[3] = read_register(ADXL345_DATAY1);
+
+  Y = (data[3] << 8) | data[2];
+
+  data[4] = read_register(ADXL345_DATAZ0);
+
+  data[5] = read_register(ADXL345_DATAZ1);
+
+  Z = (data[5] << 8) | data[4];
+
+  printf("X: %d Y: %d Z: %d \n", X * 4, Y * 4, Z * 4);
+  //printf("OFFSETX : %d OFFSETY : %d OFFSETZ : %d \n", read_register(ADXL345_OFSX),read_register(ADXL345_OFSY),read_register(ADXL345_OFSZ));
+
+  axes();
+  IOWR_ALTERA_AVALON_TIMER_STATUS(TIMER_0_BASE, 0);
+}
+
+static void irqhandler_switch(void * context) {
+  switch_value = IORD_ALTERA_AVALON_PIO_DATA(SWITCHS_BASE);
+  IOWR_ALTERA_AVALON_PIO_EDGE_CAP(SWITCHS_BASE, 0x0);
 }
